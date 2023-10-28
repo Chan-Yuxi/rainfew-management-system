6feead3516abe1ee7fc72ea1459d5ef0906136e5
Saturn Savage <2438149743@qq.com>
2023-10-28 22:21:39 +0800

    fix

--- src/main.tsx
+++ src/main.tsx
@@ -4,12 +4,10 @@ import App from "./App.tsx";
 
 import { BrowserRouter } from "react-router-dom";
 
-<<<<<<< HEAD
 import store from "@/store";
 import { Provider } from "react-redux";
-=======
+
 import "@/i18n";
->>>>>>> feature/setupI18n
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
