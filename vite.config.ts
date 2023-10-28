720a050020409101fb2ddf764bf9fce452505cc4
Saturn Savage <2438149743@qq.com>
2023-10-29 00:07:52 +0800

    set up mock with vite-plugin-mock

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,23 @@
-import { defineConfig } from "vite";
+import type { UserConfig } from "vite";
 import path from "path";
 
 import react from "@vitejs/plugin-react";
+import { viteMockServe } from "vite-plugin-mock";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+export default (): UserConfig => {
+  return {
+    plugins: [
+      react(),
+      viteMockServe({
+        mockPath: "./src/mock",
+        enable: true,
+      }),
+    ],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
     },
-  },
-});
+  };
+};
